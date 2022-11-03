8762f980d3a22399b1f59d8a2eb53c6e9c70dc2c
Adriel Baez <adriel.baez@tiarg.com.ar>
2022-11-02 21:26:28 -0300

    [add] fake data

---
+++ src/constants/onboardingScreens.ts
@@ -0,0 +1,19 @@
+export const onboardingScreens = [
+  {
+    title: 'CONTROL DEL VIENTO',
+    description:
+      'Conocé minuto a minuto el estado del viento en el Planeta Marte',
+    icon: 'cloudy-night',
+  },
+  {
+    title: 'CONTROL DEL CLIMA',
+    description: 'Llevá el control del clima Marciano en tiempo Real',
+    icon: 'thunderstorm',
+  },
+  {
+    title: 'CONTROL DE LA VIDA',
+    description:
+      'Enterate de todas las novedades de Nasa, la vida en Marte se encuentra más cerca de lo que pensabas.',
+    icon: 'rocket',
+  },
+];
