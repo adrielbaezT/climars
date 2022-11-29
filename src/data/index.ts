5932a5b0306f704deffd71fbaa96add5a65c6f2d
Adriel Baez <adriel.baez@tiarg.com.ar>
2022-11-29 20:26:55 -0300

    [modified] fake data

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+import {User} from 'features/authentication';
+
+export const user: User = {
+  email: 'samuel.burgueno@tiarg.com.ar',
+  emailVerified: null,
+  first_name: 'Samuel',
+  id: 2,
+  image: null,
+  language: null,
+  last_name: 'Burgueño',
+  name: 'Samuel Burgueño',
+  password: 'Tiarg2020',
+  token:
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7Im5hbWUiOiJTYW11ZWwgQnVyZ3Vlw7FvIiwiZW1haWwiOiJzYW11ZWwuYnVyZ3Vlbm9AdGlhcmcuY29tLmFyIiwiaW1hZ2UiOm51bGwsInN0cl91c2VySWQiOjJ9LCJleHBpcmVzIjoiMjAyMi0xMi0xNFQxNTo1Njo0OC41MjlaIn0.AWPfDHAgapfgWQD5dN0zilzl0v2c0x5bhvdp6UoCTGY',
+  username: null,
+};
